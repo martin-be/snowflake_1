1e88e18dec5300a85ebc3f8ab617bf410baeb058
martin-be <70891695+martin-be@users.noreply.github.com>
2023-10-10 14:48:12 +0200

    Update streamlit_app.py
    add missing brackets

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,15 +43,15 @@ except URLError as e:
 
 streamlit.header("The fruit load list contains:")
 # snowflake-related functions
-def get_fruit_load_list(my_cnx):
-    with my_cnx.cursor as my_cur:
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * FROM fruit_load_list")
         return my_cur.fetch_all()
 
 # add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list(my_cnx)
+    my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
 # don't run anything past here while we troubleshoot
